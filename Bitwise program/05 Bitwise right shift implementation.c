33279b57774a72424659b2c8dc416685c86e10f5
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2023-08-11 21:58:58 +0530

    bitwise program

---
+++ Bitwise program/05 Bitwise right shift implementation.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+  {
+     int No1 =0, No2 =0, Res =0;
+
+      printf("\n Enter 1st Number =");
+      scanf("%d",&No1);
+      printf("\n Enter 2st Number =");
+      scanf("%d",&No2);
+
+      printf("\n===========****=========\n");
+
+      Res= No1 >> No2;
+      printf("\n Result=> %d >> %d =%d.",No1,No2,Res);
+
+      printf("\n============******===========\n");
+      getch();
+      return 0;
+
+  }
+
+
