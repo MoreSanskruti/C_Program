7c89522022e091f11a87285fa52d8c68ef891b28
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2023-08-27 22:27:19 +0530

    Add files via upload

---
+++ Basics/wap  to calculate  area of a circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    float radius;
+
+    printf("\n Enter a radius:");
+    scanf("%f",&radius);
+
+    printf("area is:%f",3.14*radius*radius);
+
+    return 0;
+
+}
