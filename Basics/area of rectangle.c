15b089ebbf93fc6431483902d5b54ff7ed40862d
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2023-09-28 10:15:04 +0530

    Add files via upload

---
+++ Basics/area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int l=10, b=10;
+
+    printf("\n Area of Rectangle is : %d",l*b);
+    printf("\n perimeter of rectangle is :%d",2*(l+b));
+
+    return 0;
+}
