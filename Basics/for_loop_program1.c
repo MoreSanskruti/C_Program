873590cd27cef3b5e42ca0d0bd31a2b65524989e
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-01-12 22:23:56 +0530

    Add files via upload

---
+++ Basics/for_loop_program1.c
@@ -0,0 +1,14 @@
+
+// Print numbers from 1 to 100
+#include <stdio.h>
+
+int main()
+ {
+  int i;
+
+  for (i = 1; i <= 100; i++)
+  {
+    printf("%d \n ", i);
+  }
+  return 0;
+}
---
+++ Basics/for_loop_program2.c
@@ -0,0 +1,15 @@
+
+// Print letters from A to Z
+#include <stdio.h>
+
+int main()
+ {
+  char ch;
+
+  for (ch = 'A'; ch <= 'Z'; ch++)
+  {
+    printf("%c \n ", ch);
+  }
+  return 0;
+}
+
