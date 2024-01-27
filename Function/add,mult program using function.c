5ca4c02eeba98f100c29dbfcaf7376e1205f0385
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-01-27 16:59:25 +0530

    Add files via upload

---
+++ Function/add,mult program using function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int mult(int a,int b)
+{
+    return a*b;
+}
+int add(int x,int y)
+{
+    return x+y;
+}
+void main()
+{
+    printf(" product is %d",mult(5,10));
+    printf(" sum is %d",add(10,10));
+
+}
