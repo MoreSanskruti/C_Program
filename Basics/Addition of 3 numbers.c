f811730f63bafcd0c79d23e76a55a90c7e6c47e9
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-02-01 20:59:19 +0530

    Add files via upload

---
+++ Basics/Addition of 3 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n1,n2,n3,sum;
+
+    printf("Enter Three Numbers:");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    sum=n1+n2+n3;
+    printf("sum=%d",sum);
+    getch();
+    return 0;
+}
