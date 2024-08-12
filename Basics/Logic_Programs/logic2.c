8328cc04ab6523d19e91e0826fc019fbd50a52f6
MoreSanskruti <127648880+MoreSanskruti@users.noreply.github.com>
2024-08-12 23:15:09 +0530

    Add files via upload

---
+++ Basics/Logic_Programs/logic2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int x = -2;
+
+    while(x++ || x==0)
+    {
+        printf(" \n %d",x);
+    }
+    getch();
+    return 0;
+}
---
+++ Basics/Logic_Programs/logic3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int a =0,b=1;
+
+    if(a || ++b);
+    {
+        printf(" \n %d",b);
+    }
+
+    getch();
+    return 0;
+}
